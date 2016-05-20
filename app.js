3e5fb5b1377404260ce33ff21a17e8948b4e51ed
Kyle Nguyen <kyle@felizz.com>
2016-05-20 14:52:30 +0700

    Adding Fav icon

--- app.js
+++ app.js
@@ -19,8 +19,6 @@ require('./config/database-connect');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(require('morgan')('combined', {"stream": logger.stream}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
