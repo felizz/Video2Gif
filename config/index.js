20434c064b86062491190aca279c7fe41df1f7b2
Kyle Nguyen <kyle@felizz.com>
2016-05-22 12:45:37 +0700

    Further fix

--- config/index.js
+++ config/index.js
@@ -2,15 +2,12 @@
  * Created by kyle on 29/12/15.
  */
 var config = require('./' + (process.env.NODE_ENV || 'development') + '.json');
-var fs = require('fs');
-fs.stat('~/.aws/aws_credentials', function (err, stats){
-    if(err || !stats.isFile()){
-        return;
-    }
+
+if(!config.AWS.api_key){
     var aws_credentials  = require('~/.aws/aws_credentials');
     config.AWS.api_key = aws_credentials.api_key;
     config.AWS.api_secret = aws_credentials.api_secret;
     logger.info('AWS Credentials loaded from ~/.aws/aws_credentials');
-});
+}
 
 module.exports = config;
