058683f01da70f88199e8fc69b78dfb2097ef290
Kyle Nguyen <kyle@felizz.com>
2016-05-22 10:15:54 +0700

    Support picking AWS credentials from environment

--- config/index.js
+++ config/index.js
@@ -1,4 +1,16 @@
 /**
  * Created by kyle on 29/12/15.
  */
-module.exports = require('./' + (process.env.NODE_ENV || 'development') + '.json');
+var config = require('./' + (process.env.NODE_ENV || 'development') + '.json');
+
+if(process.env.AWS_API_KEY){
+    config.AWS.api_key = process.env.AWS_API_KEY;
+    logger.info('AWS api key picked from environment');
+}
+
+if(process.env.AWS_API_SECRET){
+    config.AWS.api_secret = process.env.AWS_API_SECRET;
+    logger.info('AWS api secret picked from environment');
+}
+
+module.exports = config;
