8740f03ea5c81018b7bfc00a4d6f19fc52662d28
Kyle Nguyen <kyle@felizz.com>
2016-05-22 12:58:12 +0700

    Fi

--- config/index.js
+++ config/index.js
@@ -2,9 +2,10 @@
  * Created by kyle on 29/12/15.
  */
 var config = require('./' + (process.env.NODE_ENV || 'development') + '.json');
+var logger = require('utils/logger');
 
 if(!config.AWS.api_key){
-    var aws_credentials  = require('~/.aws/aws_credentials');
+    var aws_credentials  = require(process.env.HOME + '/.aws/aws_credentials');
     config.AWS.api_key = aws_credentials.api_key;
     config.AWS.api_secret = aws_credentials.api_secret;
     logger.info('AWS Credentials loaded from ~/.aws/aws_credentials');
