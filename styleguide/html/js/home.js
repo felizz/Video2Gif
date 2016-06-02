3b652a6f356b136586ac3b219f65003b389da50a
mtuan94 <tuan.nguyen@felizz.com>
2016-06-02 10:02:18 +0700

    adding howe page - love not run

--- styleguide/html/js/home.js
+++ styleguide/html/js/home.js
@@ -55,3 +55,39 @@ $.Home.prototype = (function() {
         }
     };
 }(jQuery));
+
+function loveItem(imageID) {
+    alert(imageID);
+    $(this).addClass("abc");
+    if(this.hasClass("loveIcon")){
+        alert(this.class);
+    }
+    /*
+
+
+    $.ajax({
+        url: 'api/v1/image/'+imageID+'/love',
+        type: "POST",
+        data: {
+            love_val: 1
+        },
+        success: function(data) {
+            if(window.loved){
+                $("#loveAction").html("<i id=\"heart_icon\" class=\"fa fa-heart fa-lg\" aria-hidden=\"true\" style=\"color: #3d5fa1;\"></i>");
+                window.loved=0;
+            }else{
+                $("#loveAction").html("<i id=\"heart_icon\" class=\"fa fa-heart fa-lg\" aria-hidden=\"true\" style=\"color: #babcbf;\"></i>");
+                window.loved=1;
+            }
+            $('#loveNum').html(data.love_count);
+
+        },
+        error: function () {
+            alert("Hệ thống quá tải, Xin vui lòng thử lại!");
+        }
+    }); */
+}
+
+function foo(){
+    $(this).addClass("abc");
+}
