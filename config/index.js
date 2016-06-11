64a81974a08473e98503c3352a9e2f2f140477d8
Kyle Nguyen <kyle@felizz.com>
2016-06-11 23:14:04 +0700

    Proper log level setup for logging

--- config/index.js
+++ config/index.js
@@ -2,13 +2,11 @@
  * Created by kyle on 29/12/15.
  */
 var config = require('./' + (process.env.NODE_ENV || 'development') + '.json');
-var logger = require('utils/logger');
 
 if(!config.AWS.api_key){
     var aws_credentials  = require(process.env.HOME + '/.aws/aws_credentials');
     config.AWS.api_key = aws_credentials.api_key;
     config.AWS.api_secret = aws_credentials.api_secret;
-    logger.info('AWS Credentials loaded from ~/.aws/aws_credentials');
 }
 
 module.exports = config;
